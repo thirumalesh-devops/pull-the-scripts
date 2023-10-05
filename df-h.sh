55522b63c68905e1dbc8a8c1bba43d87fa13a221
Thirumalesh.v <110015981+vtiru05@users.noreply.github.com>
2023-10-05 12:53:21 +0530

    Update df-h.sh

--- df-h.sh
+++ df-h.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo echo "print the fs type,        mount point     used %"
-sudo echo "------------------------------------>>>> hai...! program in process"
 sudo df -h | awk 'NR>1 {print "Filesystem: "$1, "Mount Point: "$6, "Used: "$5}'
 sudo echo "i love you............!"
